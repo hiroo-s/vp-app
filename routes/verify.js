43b3aeed012a584f43f429e9c05cb87c92d88022
Hiroo Shirasaki <sirasaki@iij.ad.jp>
2022-09-21 12:53:26 +0900

    console print vc subject

--- routes/verify.js
+++ routes/verify.js
@@ -3,24 +3,25 @@ var router = express.Router();
 const didJWT = require('did-jwt');
 const resolver = require('did-resolver');
 const ION = require('@decentralized-identity/ion-tools');
+const webResolver = require('web-did-resolver')
 
 router.post('/', function(req, res, next) {
   let vpToken = req.body.vp_token;
-  console.log(vpToken);
 
   let p = didJWT.verifyJWT(vpToken, {
-    skewTime: 5184000,
     audience: 'did:web:zkp-ld.org',
     resolver: new resolver.Resolver({'ion': ION.resolve})
   });
   p.then(r => {
-      let jwt = r.jwt;
-      console.log('jwt: ' + jwt);
-      let p = didJWT.verifyJWT(jwt, {
-        skewTime: 5184000,
-        resolver: new resolver.Resolver({'ion': ION.resolve})
+      let vc = r.payload.vp.verifiableCredential;
+
+      let p = didJWT.verifyJWT(vc[0], {
+        resolver: new resolver.Resolver({'ion': ION.resolve, ...webResolver.getResolver()})
       });
-      p.then(r => {console.log(r)})
+      p.then(r => {
+        let sub = r.payload.vc.credentialSubject;
+        console.log(sub);
+      });
     });
 
   res.send('OK');
