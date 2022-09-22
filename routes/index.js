adc05ff1d14ffe20195d940f8ad4080419dad877
Hiroo Shirasaki <sirasaki@iij.ad.jp>
2022-09-22 14:47:57 +0900

    fix typo

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const { v4: uuidv4 } = require('uuid');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.app.locals.vpResult);
   param = {'uuid': uuidv4()};
   res.render('index', param);
 });
