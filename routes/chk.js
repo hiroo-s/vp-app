5778b2ab28498f8d15891473442df8a0a17784c5
Hiroo Shirasaki <sirasaki@iij.ad.jp>
2022-09-22 11:38:46 +0900

    debug log

--- routes/chk.js
+++ routes/chk.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   let uuid = req.params.id;
+  console.log(uuid);  
+  console.log(req.app.locals.verified[uuid]);
 
   if (!uuid || !req.app.locals.verified[uuid]) {
-    req.app.locals.count++;
     res.send('NG');
     return;
   }
